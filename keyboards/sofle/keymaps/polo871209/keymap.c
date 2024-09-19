72afa3906fd45a8e721b0b75dcd13e14fa2fba77
PoLo <qazh0123@gmail.com>
2024-09-19 17:08:03 +0800

    update key and drawing

--- keyboards/sofle/keymaps/polo871209/keymap.c
+++ keyboards/sofle/keymaps/polo871209/keymap.c
@@ -23,38 +23,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|  play |    | mute  |------+------+------+------+------+------|
 * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
 * `-----------------------------------------/       /     \      \-----------------------------------------'
-*            |Hyper | LAltc | LCTR| LGUI | / SPACE /       \ Enter\  |_ARROW| _NUM | LCTR |  DEL |
-*            |      |       |     |      |/       /         \      \ |      |      | Space|      |
-*            `----------------------------------'           '------''---------------------------'
+*            |Hyper | LAltc | LCTR| LGUI | / SPACE /       \ Enter\  |_ARROW| _NUM | RCTR | DEL |
+*            `------------------------------------'         '-----------------------------------'
 */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,      KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-              KC_HYPR,KC_LEFT_ALT,KC_LCTL, KC_LGUI, KC_SPC,           KC_ENT, MO(_ARROW), MO(_NUMBER), LCTL(KC_SPC), LGUI(KC_BSPC)
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,  KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+              KC_HYPR,KC_LEFT_ALT,KC_LCTL, KC_LGUI, KC_SPC,       KC_ENT, MO(_ARROW), MO(_NUMBER), KC_RCTL, LGUI(KC_BSPC)
 ),
 
 /* ARROW
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  <>  |  F11 |  F12 |   [  |   ]  |   \  |                    |      | HOME |  Up  |  END | Pause|  BS  |
+ * |      |  F11 |  F12 |   [  |   ]  |   \  |                    |      | HOME |  Up  |  END |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |  F13 |  F14 |   {  |   }  |   |  |-------.    ,-------|  PUp | Left | Down | Rigth| PDown|      |
- * |------+------+------+------+------+------|  play |    | mute  |------+------+------+------+------+------|
- * |LShift|  F15 |  F16 |   <  |   >  |   =  |-------|    |-------|   +  | CLeft|      |CRigth|      |RShift|
+ * |      |  F13 |  F14 |   {  |   }  |   |  |-------.    ,-------|  PUp | Left | Down | Rigth| PDown|      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |  F15 |  F16 |   <  |   >  |   =  |-------|    |-------|   +  | CLeft|      |CRigth|      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |Enter | LAltc | LCTR| LGUI | / SPACE /       \ Enter\  |_ARROW| _NUM | LCTR |  DEL | 
- *            |      |      |      |      |/       /         \      \ |      |      | Space|      |
- *            `----------------------------------'           '------''---------------------------'
+ *            |      |      |      |      | /       /       \      \ |      |      |     |      |
+ *            `------------------------------------'         '----------------------------------'
  */
 [_ARROW] = LAYOUT(
-  XXXXXXX,  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                         KC_F6,         KC_F7,   KC_F8,         KC_F9,  KC_F10,  XXXXXXX,
-  _______, KC_F11, KC_F12,  KC_LBRC, KC_RBRC, KC_BSLS,                       XXXXXXX,       KC_HOME,   KC_UP,        KC_END, XXXXXXX,  _______,
-  _______, KC_F13, KC_F14, KC_LCBR,  KC_RCBR, KC_PIPE,                       KC_PGUP,       KC_LEFT, KC_DOWN,       KC_RGHT, KC_PGDN,  XXXXXXX,
-  _______, KC_F15, KC_F16,   KC_LT,    KC_GT,  KC_EQL, _______,     _______, KC_PPLS, LALT(KC_LEFT), XXXXXXX, LALT(KC_RGHT), XXXXXXX,  _______,
+  _______,  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                         KC_F6,         KC_F7,   KC_F8,         KC_F9,  KC_F10,  _______,
+  _______, KC_F11, KC_F12,  KC_LBRC, KC_RBRC, KC_BSLS,                       _______,       KC_HOME,   KC_UP,        KC_END, _______,  _______,
+  _______, KC_F13, KC_F14, KC_LCBR,  KC_RCBR, KC_PIPE,                       KC_PGUP,       KC_LEFT, KC_DOWN,       KC_RGHT, KC_PGDN,  _______,
+  _______, KC_F15, KC_F16,   KC_LT,    KC_GT,  KC_EQL, _______,     _______, KC_PPLS, LALT(KC_LEFT), _______, LALT(KC_RGHT), _______,  _______,
                 _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______
 ),
 
@@ -62,21 +60,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------_-----------.                    ,-----------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |                    |  ^   |  &   |  *   |  (   |   )  |  _   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TRNS |      |      |   [  |   ]  |   \  |                    |      |  7   |  8   |  9   |      |  Bs  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |      |      |   {  |   }  |   |  |-------.    ,-------|      |  4   |  5   |  6   |  :   |  "   |
- * |------+------+------+------+------+------|  play |    | mute  |------+------+------+------+------+------|
- * |LShift|      |      |   <  |   >  |   =  |-------|    |-------|   0  |  1   |  2   |  3   |  ?   |RShift|
+ * |      |      |      |   [  |   ]  |   \  |                    |      |  7   |  8   |  9   |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+----- |
+ * |      |      |      |   {  |   }  |   |  |-------.    ,-------|      |  4   |  5   |  6   |  :   |  "   |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |   <  |   >  |   =  |-------|    |-------|   0  |  1   |  2   |  3   |  ?   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |Enter | LAltc | LCTR| LGUI | / SPACE /       \ Enter\  |_ARROW| _NUM | LCTR |  DEL | 
- *            |      |      |      |      |/       /         \      \ |      |      | Space|      |
- *            `----------------------------------'           '------''---------------------------'
+ *            |      |      |      |      | /       /       \      \ |      |      |     |      |
+ *            `------------------------------------'         '----------------------------------'
  */
 [_NUMBER] = LAYOUT(
   KC_TILD, KC_EXLM,    KC_AT, KC_HASH,   KC_DLR, KC_PERC,                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
-  _______, XXXXXXX,  XXXXXXX, KC_LBRC,  KC_RBRC, KC_BSLS,                           XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX, _______,
-  _______, XXXXXXX,  XXXXXXX, KC_LCBR,  KC_RCBR, KC_PIPE,                           XXXXXXX,    KC_4,    KC_5,    KC_6, KC_COLN, KC_DQUO,
-  _______, XXXXXXX,  XXXXXXX,   KC_LT,    KC_GT,  KC_EQL,  _______,        _______,    KC_0,    KC_1,    KC_2,    KC_3, KC_QUES, _______,
+  _______, _______,  _______, KC_LBRC,  KC_RBRC, KC_BSLS,                           _______,    KC_7,    KC_8,    KC_9, _______, _______,
+  _______, _______,  _______, KC_LCBR,  KC_RCBR, KC_PIPE,                           _______,    KC_4,    KC_5,    KC_6, KC_COLN, KC_DQUO,
+  _______, _______,  _______,   KC_LT,    KC_GT,  KC_EQL,  _______,        _______,    KC_0,    KC_1,    KC_2,    KC_3, KC_QUES, _______,
                     _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______
 )
 };
@@ -263,7 +260,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_
 
 /* BONGO CAT START */
 static void render_anim(void) {
-    static const char PROGMEM anim[] = { 
+    static const char PROGMEM anim[] = {
         0x00, 0xc0, 0x3e, 0x01, 0x00, 0x00, 0x00, 0xc0, 0xfc, 0x03, 0x00, 0x03, 0x0c, 0x30, 0xc0, 0x00,
         0xe1, 0x1e, 0x00, 0xc0, 0xbc, 0x83, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x3c, 0x03, 0x00, 0x00, 0x00, 0x80, 0x78, 0x87, 0x00, 0x03, 0x0c, 0x30, 0xc0, 0x00, 0xe0, 0x1f,
@@ -381,6 +378,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 #ifdef ENCODER_ENABLE
+#define ENCODER_THRESHOLD 2
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
@@ -389,9 +387,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code(KC_VOLD);
         }
-        
+
     } else if (index == 1) {
-        
+
         if (clockwise) {
             tap_code16(KC_RGHT);
         } else {
